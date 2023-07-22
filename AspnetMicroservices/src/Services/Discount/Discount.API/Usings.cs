d717fcb6399f3954f6b55313c5c83c2365bd8a51
anh.le <anh.le@s3corp.com.vn>
2023-07-23 00:11:33 +0700

    CRUD discount api, and containerization discount api

---
+++ AspnetMicroservices/src/Services/Discount/Discount.API/Usings.cs
@@ -0,0 +1,7 @@
+﻿global using Dapper;
+global using Discount.API.Entities;
+global using Discount.API.Repositories.Implementation;
+global using Discount.API.Repositories.Interfaces;
+global using Microsoft.AspNetCore.Mvc;
+global using Npgsql;
+global using System.Net;
